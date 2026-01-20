63861fb8e612eee2c7de94dc143fa2579efa691a
Orlando Verdin <git@orli.land>
2026-01-19 23:40:11 -0800

    add styles to footer for tablet and desktop

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -8,16 +8,18 @@ import SocialLinks from "../../components/SocialLinks";
 function Footer({ isDark }) {
   return (
     <footer className={`footer ${isDark ? "is-dark" : ""}`}>
-      <img
-        className="footer__logo"
-        src={isDark ? LogoLight : LogoDark}
-        alt="Modern Art Gallery Logo"
-      />
-      <p className="footer__description footer-text">
-        The Modern Art Gallery is free to all visitors and open seven days a
-        week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
-      </p>
-      <SocialLinks />
+      <div className="footer__wrapper">
+        <img
+          className="footer__logo"
+          src={isDark ? LogoLight : LogoDark}
+          alt="Modern Art Gallery Logo"
+        />
+        <p className="footer__description footer-text">
+          The Modern Art Gallery is free to all visitors and open seven days a
+          week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+        </p>
+        <SocialLinks />
+      </div>
     </footer>
   );
 }
